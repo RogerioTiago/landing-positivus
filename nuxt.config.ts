fedd1d923dba0a8a6a9c69f079bc40a9ed98e52e
Rogerio Tiago <tiago.gregorio31@gmail.com>
2024-11-23 21:06:24 -0300

    Nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+
+  // Compatibilidade com Nuxt4
+  future: {
+    compatibilityVersion: 4,
+  },
 })
