ecf95ec7c0125c697895dc773775b43261116133
Rogerio Tiago <teste@teste.com.br>
2025-02-09 13:08:51 -0300

    mudança contato

--- app/components/Section/Contact/index.vue
+++ app/components/Section/Contact/index.vue
@@ -1,17 +1,19 @@
 <template>
   <section class="contact">
     <div class="container">
-      <div class="texto">
-        <h2 class="title">Let’s make things happen</h2>
-        <p class="subtitle">
-          Contact us today to learn more about how our digital marketing
-          services can help your business grow and succeed online.
-        </p>
+      <div class="card">
+        <div class="texto">
+          <h2 class="title">Let’s make things happen</h2>
+          <p class="subtitle">
+            Contact us today to learn more about how our digital marketing
+            services can help your business grow and succeed online.
+          </p>
+        </div>
+        <div class="image">
+          <img class="arrow" src="/assets/imagens/contatosimgs.jpg" />
+        </div>
+        <button class="button">Get your proposal</button>
       </div>
-      <div class="image">
-        <img class="arrow" src="/assets/imagens/contatosimgs.jpg" />
-      </div>
-      <button class="button">Get your proposal</button>
     </div>
   </section>
 </template>
