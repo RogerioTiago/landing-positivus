aa2338aafedfde57e6f71d6cc95027e2c07cf37a
Rogerio Tiago <teste@teste.com.br>
2025-02-10 16:26:37 -0300

    alterar studies

--- app/components/Section/Contact/index.vue
+++ app/components/Section/Contact/index.vue
@@ -8,11 +8,11 @@
             Contact us today to learn more about how our digital marketing
             services can help your business grow and succeed online.
           </p>
+          <button class="button">Get your proposal</button>
         </div>
         <div class="image">
           <img class="arrow" src="/assets/imagens/contatosimgs.jpg" />
         </div>
-        <button class="button">Get your proposal</button>
       </div>
     </div>
   </section>
