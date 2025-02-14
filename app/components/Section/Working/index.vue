abffb5270c314155e31ec113872444b6711e6cb6
Rogerio Tiago <teste@teste.com.br>
2025-02-14 18:43:58 -0300

    alterar working

--- app/components/Section/Working/index.vue
+++ app/components/Section/Working/index.vue
@@ -19,26 +19,32 @@
             will allow us to understand your needs and tailor our services to
             best fit your requirements.
           </div>
+          <img class="plus" src="/assets/imagens/plus.png" />
         </div>
         <div class="card">
           <p>02</p>
           <p>Research and Strategy Development</p>
+          <img class="plus" src="/assets/imagens/plus.png" />
         </div>
         <div class="card">
           <p>03</p>
           <p>Implementation</p>
+          <img class="plus" src="/assets/imagens/plus.png" />
         </div>
         <div class="card">
           <p>04</p>
           <p>Monitoring and Optimization</p>
+          <img class="plus" src="/assets/imagens/plus.png" />
         </div>
         <div class="card">
           <p>05</p>
           <p>Reporting and Communication</p>
+          <img class="plus" src="/assets/imagens/plus.png" />
         </div>
         <div class="card">
           <p>06</p>
           <p>Continual Improvement</p>
+          <img class="plus" src="/assets/imagens/plus.png" />
         </div>
       </div>
     </div>
